b76c5b3b3d52ea0242fdef14f6bfd5f374e0c9b9
effyiffurby <iffah.ada@gmail.com>
2024-01-16 19:13:07 +0800

    render sightings

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import logo from "/logo.png";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  //state to store all sightings data from API
+  const [sighting, setSighting] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const loadData = async () => {
+      //till data is fetched, show loading page
+      setLoading(true);
+
+      //fetching data from backend
+      const response = await axios.get("http://[::1]:3000/sightings");
+      console.log(response.data);
+
+      //store fetched data in state
+      setSighting(response.data);
+
+      setLoading(false);
+    };
+
+    loadData();
+  }, []);
+
   return (
     <>
       <div>
@@ -13,6 +36,16 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        {loading ? (
+          <h4>Loading, please wait</h4>
+        ) : (
+          sighting.map((item) => (
+            <p key={item.REPORT_NUMBER}>
+              {item.YEAR}
+              {item.LOCATION_DETAILS}
+            </p>
+          ))
+        )}
       </div>
     </>
   );
